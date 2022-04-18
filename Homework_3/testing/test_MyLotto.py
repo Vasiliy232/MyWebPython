adb999188311e1ee8ab18f6afc6dc78b913938d1
Lisovskii Vasilii <lisovskii@biocad.ru>
2022-04-18 18:23:10 +0300

    Homework 3: Tests

--- Homework_3/testing/test_MyLotto.py
+++ Homework_3/testing/test_MyLotto.py
@@ -1,5 +1,48 @@
-from Homework_2.MyLotto import Barrel
+from Homework_2.MyLotto import Card
+from pytest import fixture, raises
+from random import choice
 
 
-class TestBarrel:
+@fixture
+def card_instance():
+    card = Card()
+    return card, card.card_list
+
+
+def exception_list(numb_list):
+    """ Возвращает список из чисел, исключая числа из передаваемого списка """
+    result_list = []
+    for i in range(1, 91):
+        if i not in numb_list:
+            result_list.append(i)
+    return result_list
+
+
+class TestCard:
+    def test_contain(self, card_instance):
+        num = choice(card_instance[1])  # Выбираем случайное число из карточки
+        assert num in card_instance[1]
+
+    def test_cross_num(self, card_instance):
+        num = choice(card_instance[1])
+        res = card_instance[0].cross_num(num)
+        assert res == -1
+
+    def test_cross_num__raises(self, card_instance):
+        except_card_list = exception_list(card_instance[1])
+        num = choice(except_card_list)
+        with raises(ValueError) as exc_info:
+            card_instance[0].cross_num(num)
+
+        assert str(exc_info.value) == f"Номера нет в карточке: {num}"
+
+    def test_closed(self, card_instance):
+        for num in card_instance[1]:
+            if num != 0:
+                card_instance[0].cross_num(num)
+        res = card_instance[0].closed()
+        assert res
+
+
+class TestUser:
     pass
